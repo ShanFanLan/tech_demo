119ff3dceffaa530dfff615fab71a04d4da1fbbb
ShanFanLan <slan526@aucklanduni.ac.nz>
2023-04-12 19:39:49 +1200

    Update getTableData.js

--- src/getTableData.js
+++ src/getTableData.js
@@ -3,9 +3,13 @@ export default function getTableData(favorite) {
     let tableData = [];
       
     for (let i = 0; i < favorite.length; i++) {
-      for (let j = 0; j < favorite[i]["ingredients"].length; j++)
+      for (let j = 0; j < favorite[i].ingredients.length; j++)
       tableData = [...tableData , favorite[i]["ingredients"][j]];
     }
     
     return tableData
 }
+
+// favorite[i].ingredients.length
+// favorite[i]["ingredients"][j]
+// 以上兩種表示方法都可以
